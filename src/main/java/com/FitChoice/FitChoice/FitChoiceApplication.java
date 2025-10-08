44000b0e32f3b2fcf09aeff4393a0bfa3c9f593e
Milotai Dalma Henrietta <milotaidalma@gmail.com>
2025-10-08 10:32:32 +0300

    Initial commit - Fitness Center project

---
+++ src/main/java/com/FitChoice/FitChoice/FitChoiceApplication.java
@@ -0,0 +1,13 @@
+package com.FitChoice.FitChoice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitChoiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitChoiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FitChoice/FitChoice/FitChoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FitChoice.FitChoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitChoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
